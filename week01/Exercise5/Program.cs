8cf27a41cb1050dabca502f648b7738dad643a9a
DaveFont19 <dfontes@byupathway.edu>
2025-01-11 22:29:20 -0600

    first commit

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Console.WriteLine("Making changes in this code");
     }
+
 }
