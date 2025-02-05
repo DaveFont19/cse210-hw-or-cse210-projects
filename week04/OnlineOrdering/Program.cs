43e5d6ab7cab6022a14b85cb13ce32db6554e8fc
DaveFont19 <dfontes@byupathway.edu>
2025-02-05 12:50:25 -0600

    Online Ordering Program

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Order _order1 = new Order();
+        _order1.CustomerInformation("David Fontes", "No street", "Zapopan", "jalisco", "Mexico");
+        _order1.AddProduct(150, "Milk", 3.99, 4);
+        _order1.AddProduct(125, "Eggs", 14.99, 2);
+        _order1.DisplayInformation();
+        _order1.DisplayOrder();
+        _order1.DisplaySum();
+
+        Order _order2 = new Order();
+        _order2.CustomerInformation("Alonso Luna", "No street", "Los Angeles", "California", "USA");
+        _order2.AddProduct(150, "Milk", 3.99, 4);
+        _order2.AddProduct(125, "Eggs", 14.99, 2);
+        _order2.DisplayInformation();
+        _order2.DisplayOrder();
+        _order2.DisplaySum();
+
     }
 }
