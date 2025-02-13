fa22eaebc1f20558472da6cd2b6c02e4fd204be4
DaveFont19 <dfontes@byupathway.edu>
2025-02-13 10:25:08 -0600

    W01 Assignment

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -14,27 +14,32 @@ class Program
         DisplayResult(name, square);
     }
 
-    static void DisplayMessage(){
+    static void DisplayMessage()
+    {
         Console.WriteLine("Welcome to the Program!");
     }
-    static string PromptUserName(){
+    static string PromptUserName()
+    {
         Console.WriteLine("What is your name? ");
         string name = Console.ReadLine();
         return name;
     }
-     static int PromptUserNumber(){
+    static int PromptUserNumber()
+    {
         Console.WriteLine("What is your favorite number? ");
         int number = int.Parse(Console.ReadLine());
         return number;
     }
 
-    static int SquareNumber(int number){
+    static int SquareNumber(int number)
+    {
         int square = number * number;
         return square;
     }
 
-    static void DisplayResult(string name, int square){
+    static void DisplayResult(string name, int square)
+    {
         Console.WriteLine($"{name}, the square of your number is {square}");
     }
-    
+
 }
